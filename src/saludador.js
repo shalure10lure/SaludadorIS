07b1e28175b2da3145caecf1289731d6440e8378
shalure10lure <yessicarado@gmail.com>
2026-08-24 22:04:24 -0400

    feat: implementar saludo segun la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,14 @@
 function saludar(nombre) {
-  return `Hola, ${nombre}!`;
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
+  let saludo = `Buenas noches, ${nombre}!`;
+  if (horaActual < 12) {
+    saludo = `Buenos días, ${nombre}!`;
+  }
+  if (horaActual < 19) {
+    saludo = `Buenas tardes, ${nombre}!`;
+  }
+  return saludo;
 }
 
 export default saludar;
